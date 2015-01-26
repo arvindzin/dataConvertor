e8e32efd21b6ff88f9a5cbecce5fceebb5bbfaa1
Arvind Gopalakrishnan <arvindzin@gmail.com>
2015-01-25 18:46:25 -0800

    threading for serial checker

--- serrecv.py
+++ serrecv.py
@@ -1,6 +1,25 @@
 import serial
-ser = serial.Serial('/dev/ttys1', 115200, timeout=5)
-while True:
-    x= ser.read(6)
-    print(x)
-ser.close()
+import thread
+
+ser0 = serial.Serial('/dev/ttys0', 115200, timeout=5)
+ser1 = serial.Serial('/dev/ttys1', 115200, timeout=5)
+ser2 = serial.Serial('/dev/ttys2', 115200, timeout=5)
+ser3 = serial.Serial('/dev/ttys3', 115200, timeout=5)
+
+def recvSerial(threadname, serial)
+    print "%s" % ( threadName )
+    while True:
+        x= serial.read(6)
+        print(x)
+    serial.close()
+
+try:
+    thread.start_new_thread( recvSerial, ("p0", ser0, ))
+    thread.start_new_thread( recvSerial, ("p1", ser1, ))
+    thread.start_new_thread( recvSerial, ("p2", ser2, ))
+    thread.start_new_thread( recvSerial, ("p3", ser3, ))
+except:
+    print "Error"
+    
+while 1:
+    pass
